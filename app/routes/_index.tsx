f5236895442b18d7de8993b12de4da995f16e00c
Ashley Lamont <ashley@ashl.dev>
2023-06-19 16:32:49 +1000

    minor ui fixes

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,16 +1,31 @@
-import type { V2_MetaFunction } from "@remix-run/node";
+import type { LoaderArgs, V2_MetaFunction } from "@remix-run/node";
 import OidcLoginRoute from "~/routes/login";
 import appConfig from "../../app.config";
+import { Link, Typography } from "@mui/material";
+import { isLoggedIn } from "~/user.session.server";
+import { json } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import React from "react";
 
 export const meta: V2_MetaFunction = () => [{ title: appConfig.appName }];
 
+export async function loader({request}: LoaderArgs) {
+  const loggedIn = await isLoggedIn(request);
+  return json(loggedIn);
+}
+
 export default function Index() {
+  const loggedIn = useLoaderData();
   return (
-    <main className="relative min-h-screen bg-white sm:flex sm:items-center sm:justify-center">
-      <div>
-        <h1>{appConfig.appName}</h1>
-        <OidcLoginRoute/>
-      </div>
+    <main>
+      <Typography variant="h1">Welcome to {appConfig.appName}</Typography>
+      {loggedIn ? <>
+        <Typography variant="h2">You are logged in</Typography>
+        <Link href="/app">Go to app</Link>
+      </> : <>
+        <Typography variant="h2">You are not logged in</Typography>
+        <OidcLoginRoute />
+      </>}
     </main>
   );
 }
--- app/routes/app._index.tsx
+++ app/routes/app._index.tsx
@@ -1,10 +1,6 @@
 import type { LoaderArgs } from "@remix-run/node";
 import { requireLoggedIn } from "~/user.session.server";
-import { Grid, ThemeProvider } from "@mui/material";
-import AppMenu from "~/components/appMenu";
-import { theme } from "~/muiStyle";
-import { Outlet, useLoaderData } from "@remix-run/react";
-import type { User } from "@prisma/client";
+import { useLoaderData } from "@remix-run/react";
 
 export async function loader({ request }: LoaderArgs) {
   return requireLoggedIn(request);
@@ -12,17 +8,8 @@ export async function loader({ request }: LoaderArgs) {
 
 
 export default function App_index(): JSX.Element {
-  const user: User & {fallbackProfile: string} = useLoaderData();
-  return (
-    <ThemeProvider theme={theme}>
-      <Grid container>
-        <Grid item xs={12}>
-          <AppMenu userName={user.name ?? 'Name not found'} userPhoto={user.avatarUrl} fallbackProfile={user.fallbackProfile}/>
-        </Grid>
-        <Grid item xs={12}>
-          <Outlet />
-        </Grid>
-      </Grid>
-    </ThemeProvider>
-  );
+  useLoaderData();
+  return <>
+    App.
+  </>
 }
